4e9860b8cf8c46cdd6519861893ed7418b60fc48
ArzelaAscoIi <kristof.herrmann@rwth-aachen.de>
2021-09-15 11:03:42 +0200

    added router

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import i18n from '@/plugins/i18n';
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <h1>This is an about page {{val}}</h1>
+  </v-card>
+</template>
+
+
+
+<script>
+export default {
+  name: "About",
+
+  mounted() {
+    setInterval(() => {
+      this.asdf();
+    }, 1000);
+  },
+  watch: {},
+  methods: {
+    asdf() {
+      this.val = this.val + 1;
+    }
+  },
+
+  data: () => ({
+    val: 1
+  })
+};
+</script>
