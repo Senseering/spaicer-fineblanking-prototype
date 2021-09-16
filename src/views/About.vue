a8170f196557bb223b68123c8dca925bd5a862f8
LuciaOrt <lucia.ortjohann@rwth-aachen.de>
2021-09-16 15:03:01 +0200

    UPDATE: nearly ready for presentation
    iframe does not work properly yet

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,29 +1,72 @@
 <template>
-  <v-card>
-    <h1>This is an about page {{val}}</h1>
-  </v-card>
+  <v-container fluid justify="center">
+    <v-row no-gutters justify="center">
+      <v-col>
+        <v-container>
+          Gib des Problems:
+          <v-combobox
+            v-model="request"
+            filled
+            append-icon
+            hide-details
+            dense
+            :label="$t('search.query')"
+          >
+          </v-combobox>
+          <div v-if="request != null">
+            <br />
+            <strong class="ml-3">
+              Beschreibungen aus dem Anlagetagebuch:</strong
+            >
+
+            <br />
+            <ul>
+              <li v-for="product in products" :key="product._id">
+                {{ product }}
+              </li>
+            </ul>
+          </div>
+        </v-container>
+      </v-col>
+      <v-col>
+        <v-container>
+          Betriebsanleitung:
+          <v-select
+            v-model="selectedMachine"
+            :items="maschines"
+            filled
+            label="Maschine"
+            dense
+            hide-details
+          ></v-select>
+          <div v-if="selectedMachine === 'Feintool XFT2500speed'">
+            <iframe
+              src="@assets/XFT2500speed.pdf"
+              style="width: 100%; height: 600px"
+              frameborder="0"
+            ></iframe>
+          </div>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 
 
 <script>
 export default {
-  name: "About",
-
-  mounted() {
-    setInterval(() => {
-      this.asdf();
-    }, 1000);
-  },
-  watch: {},
-  methods: {
-    asdf() {
-      this.val = this.val + 1;
-    }
-  },
+  name: "UC3",
 
   data: () => ({
-    val: 1
-  })
+    selectedMachine: null,
+    request: null,
+    maschines: ["Feintool XFT2500speed"],
+    products: [
+      "Eine zulockere Trocknerkette",
+      "Kette wieder drauf gefädelt und gespannt ",
+      "Lüfter von Trockner macht sehr starke Geräusche",
+    ],
+  }),
 };
 </script>
